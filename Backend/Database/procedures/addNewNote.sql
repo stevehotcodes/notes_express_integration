01df094ea5a72eac15f0651ba40d3cc904f2fcaf
Stephen Ondieki <stevehotcodes@gmail.com>
2023-11-03 12:08:44 +0300

    remove some data definition in addNewnote procedure

--- Backend/Database/procedures/addNewNote.sql
+++ Backend/Database/procedures/addNewNote.sql
@@ -1,9 +1,3 @@
- id VARCHAR(255) PRIMARY KEY NOT NULL,
-    noteTitle VARCHAR (255) NOT NULL,
-    noteDescription VARCHAR(255) NOT NULL,
-    createdAt DATETIME GETDATE() NOT NULL
-
-
 
 CREATE OR ALTER PROCEDURE addNewNote (
     id VARCHAR(255),
